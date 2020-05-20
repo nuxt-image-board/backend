03f206a1582a3386a7298772274d6f0e14a16f13
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-05-21 00:59:59 +0900

    API: scrapeのミス修正

--- blueprints/superuser.py
+++ blueprints/superuser.py
@@ -7,6 +7,24 @@ import subprocess
 superuser_api = Blueprint('superuser_api', __name__)
 
 
+@superuser_api.route('/gc', methods=["GET"], strict_slashes=False)
+def garbageCollect():
+    '''
+    作品のない作者 や 作品のないタグ、 登録されていないタグの情報 などを削除する
+    '''
+    deleteArtistReq = '''DELETE FROM info_artist
+    WHERE artistID NOT IN( SELECT DISTINCT artistID FROM data_illust)'''
+    deleteTagDataReq = '''DELETE FROM data_tag
+    WHERE illustID NOT IN( SELECT illustID FROM data_illust )'''
+    deleteTagInfoReq = '''SELECT * FROM info_tag
+    WHERE tagID NOT IN( SELECT tagID FROM data_tag ) AND tagID != 42'''
+    req = [deleteArtistReq, deleteTagDataReq, deleteTagInfoReq]
+    for r in req:
+        g.db.edit(r, autoCommit=False)
+    g.db.commit()
+    return {'status': 200, 'message': 'ok'}
+
+
 @superuser_api.route('/start_deploy_from_github', methods=["POST"], strict_slashes=False)
 def deployNewVersion():
     password = request.args.get('password', type=str, default="")
