71fffbc1e2512c7c6a7614af33c700360dcf80f8
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-05-26 16:26:06 +0900

    API: 通知関連の実装と微細なバグ修正

--- blueprints/db.py
+++ blueprints/db.py
@@ -13,13 +13,13 @@ class SQLHandler(object):
         else:
             self.conn.execute(sql,data)
         return self.conn.fetchall()
-        
+
     def commit(self):
         self.db.commit()
-        
+
     def rollback(self):
         self.db.rollback()
-        
+
     def edit(self,sql,data=None,autoCommit=True):
         try:
             if data == None:
@@ -33,25 +33,26 @@ class SQLHandler(object):
             print(e)
             self.db.rollback()
             return False
-            
+
     def has(self,tableName,condition,data=None):
         d = self.get("SELECT 'T' FROM %s WHERE %s"%(tableName,condition),data)
         if len(d) > 0:
             return True
         else:
             return False
-            
+
     def close(self):
         self.conn.close()
 '''
 
+
 class SQLHandler(object):
     def __init__(self,
-        file_name="***REMOVED***",
-        host="***REMOVED***",
-        port="3306",
-        user="***REMOVED***",
-        password="***REMOVED***"):
+                 file_name="***REMOVED***",
+                 host="***REMOVED***",
+                 port="3306",
+                 user="***REMOVED***",
+                 password="***REMOVED***"):
         self.db = mysql.connector.connect(
             host=host,
             port=port,
@@ -61,25 +62,25 @@ class SQLHandler(object):
         )
         self.conn = self.db.cursor()
 
-    def get(self,sql,data=None):
+    def get(self, sql, data=None):
         if data == None:
             self.conn.execute(sql)
         else:
-            self.conn.execute(sql,data)
+            self.conn.execute(sql, data)
         return self.conn.fetchall()
-        
+
     def commit(self):
         self.db.commit()
-        
+
     def rollback(self):
         self.db.rollback()
-        
-    def edit(self,sql,data=None,autoCommit=True):
+
+    def edit(self, sql, data=None, autoCommit=True):
         try:
             if data == None:
                 self.conn.execute(sql)
             else:
-                self.conn.execute(sql,data)
+                self.conn.execute(sql, data)
             if autoCommit:
                 self.db.commit()
             return True
@@ -87,14 +88,14 @@ class SQLHandler(object):
             print(e)
             self.db.rollback()
             return False
-            
-    def has(self,tableName,condition,data=None):
-        d = self.get("SELECT 'T' FROM %s WHERE %s"%(tableName,condition),data)
+
+    def has(self, tableName, condition, data=None):
+        d = self.get("SELECT 'T' FROM %s WHERE %s" %
+                     (tableName, condition), data)
         if len(d) > 0:
             return True
         else:
             return False
-            
+
     def close(self):
         self.conn.close()
-
