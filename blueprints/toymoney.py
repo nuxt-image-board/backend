4e4c4b8786c70020a127a6a2ccc251c643b2c96c
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2020-07-23 06:46:18 +0900

    API: torimochiのPUTリクエスト対応/ PYON加算処理追加

--- blueprints/toymoney.py
+++ blueprints/toymoney.py
@@ -10,12 +10,13 @@ TOYMONEY_ENDPOINT = "http://127.0.0.1:7070"
 
 @toymoney_api.route(
     '/<path:text>',
-    methods=["POST", "GET"],
+    methods=["POST", "GET", "PUT"],
     strict_slashes=False
 )
 @auth.login_required
 @apiLimiter.limit(handleApiPermission)
 def torimochi(text):
+    print(g.userID)
     toyApiKey = g.db.get(
         "SELECT userToyApiKey FROM data_user WHERE userID=%s",
         (g.userID,)
@@ -30,9 +31,16 @@ def torimochi(text):
         )
     else:
         data = request.get_json()
-        resp = requests.post(
-            TOYMONEY_ENDPOINT + path,
-            json=data,
-            headers=headers
-        )
+        if request.method == "POST":
+            resp = requests.post(
+                TOYMONEY_ENDPOINT + path,
+                json=data,
+                headers=headers
+            )
+        else:
+            resp = requests.put(
+                TOYMONEY_ENDPOINT + path,
+                json=data,
+                headers=headers
+            )
     return (resp.text, resp.status_code, resp.headers.items())
